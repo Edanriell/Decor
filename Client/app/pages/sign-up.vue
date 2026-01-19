32614e68bd411845e722b724da2632f767c9c645
Edanriell <Edanriell@gmail.com>
2026-01-19 07:50:24 +0200

    feature(create-account): created desktop version

--- Client/app/pages/sign-up.vue
+++ Client/app/pages/sign-up.vue
@@ -7,7 +7,9 @@ definePageMeta({
 </script>
 
 <template>
-	<section class="h-full pt-6 pl-6 pr-6 pb-15 tablet:pt-12 tablet:pl-30 tablet:pr-30 tablet:pb-18.25">
+	<section
+		class="h-full pt-6 pl-6 pr-6 pb-15 tablet:pt-12 tablet:pl-30 tablet:pr-30 tablet:pb-18.25 desktop:pt-[unset] desktop:pl-20 desktop:pr-20 desktop:pb-[unset] desktop:flex desktop:flex-col desktop:justify-center desktop:items-center"
+	>
 		<CreateAccount />
 	</section>
 </template>
