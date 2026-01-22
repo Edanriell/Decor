7f42003f3c06445c2200c37b57ff2faa6b506cc7
Edanriell <Edanriell@gmail.com>
2026-01-22 21:06:27 +0200

    feature(otp): tablet-desktop

--- Client/app/features/reset-password/reset-password.vue
+++ Client/app/features/reset-password/reset-password.vue
@@ -173,7 +173,7 @@ type Step = "email" | "otp" | "new-password";
 			</button>
 		</form>
 		<p
-			class="block text-center mt-3 font-[Urbanist] font-normal text-[0.75rem] leading-[1.6] capitalize text-(--colors-texts-text---high-emphasis) desktop:mt-5"
+			class="block text-center mt-3 font-[Urbanist] font-normal text-[0.75rem] leading-[1.6] capitalize text-(--colors-texts-text---high-emphasis) desktop:mt-3"
 		>
 			Didn’t receive the mail?
 			<NuxtLink
